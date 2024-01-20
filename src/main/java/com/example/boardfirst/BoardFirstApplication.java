743bfdb3d554b82b7a990ff134e2a564ca243447
pxvnc1617 <chsong1617@nate.com>
2024-01-20 18:01:42 +0900

    Backend with Frontend

---
+++ src/main/java/com/example/boardfirst/BoardFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boardfirst/BoardFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
